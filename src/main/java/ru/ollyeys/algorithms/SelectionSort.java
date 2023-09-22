568d4e36621611075e4d9e7685ed4ddff5c932ee
Olya Panasyuzhenkova <panasyuzhenkova@gmail.com>
2023-09-22 21:03:08 +0300

    add SelectionSort.java and README.md

---
+++ src/main/java/ru/ollyeys/algorithms/SelectionSort.java
@@ -0,0 +1,27 @@
+package ru.ollyeys.algorithms;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    private static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minInd = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minInd] > array[j]) {
+                    minInd = j;
+                }
+            }
+            if (minInd != i) {
+                int temp = array[i];
+                array[i] = array[minInd];
+                array[minInd] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] testArray = {104, 12, 45, 44, 1, 4, 9, 10};
+        selectionSort(testArray);
+        System.out.println(Arrays.toString((testArray)));
+    }
+}
